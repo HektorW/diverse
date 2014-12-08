8c87a5d4078bd32eefa47f81dc0a6a79b66e31f3
Hektor Wallin <hektorw@gmail.com>
2014-12-08 23:06:49 +0100

    some functionality, bugs included

--- polymer-deploy/scripts/main.js
+++ polymer-deploy/scripts/main.js
@@ -3,21 +3,31 @@
 
 	$(document).on('polymer-ready', function() {
 
+		$('#drawerPanel').on('core-responsive-change', function() {
+			$('#navicon').get(0).hidden = !$('#drawerPanel').get(0).narrow;
+		});
+		$('#navicon').get(0).hidden = !$('#drawerPanel').get(0).narrow;
+
 		$('#navicon').on('click', function() {
 			$('#drawerPanel').get(0).togglePanel();
 		});
 
 		showSectionItem('configurator-type', 'chanel');
 
-		$('core-item').on('click', function(event) {
+		$('paper-item[data-configurator]').on('click', function(event) {
 			var $target = $(event.target);
 
-			var type = $target.attr('label').toLowerCase();
+			var type = $target.attr('data-type').toLowerCase();
 
 			showSectionItem('configurator-type', type);
+			showSectionItem('tab-content', dataElem('configurator-type', type).find('section').attr('data-type'));
+
+			if (!$('#drawerPanel').get(0).narrow) {
+				$('#drawerPanel').get(0).togglePanel();
+			}
 		});
 
-		showSectionItem('tab-content', 'version');
+		showSectionItem('tab-content', 'driveline');
 
 		$('[data-version-tab]').click(function(event) {
 			var $target = $(event.target);
@@ -30,6 +40,9 @@
 
 	function showSectionItem(type, item) {
 		$('[data-' + type + ']').hide().filter('[data-type="' + item + '"]').show();	
+	}
+	function dataElem(data, val) {
+		return val ? $('[data-' + data + '="' + val + '"]') : $('[data-' + data + ']');
 	}
 
 
