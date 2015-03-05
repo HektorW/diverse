fb2acc008b919eca56c8c53fa6771207e3944094
Hektor Wallin <hektorw@gmail.com>
2015-03-05 23:44:49 +0100

    Heheh bouncy

--- bounce/404.js
+++ bounce/404.js
@@ -77,9 +77,10 @@
 
 	var TWOPIE = Math.PI * 2;
 
-	function randRange(min, max) {
-		return min + (Math.random() * Math.abs(max-min));
-	}
+	function randItem (list) { return list[Math.floor(randRange(0, list.length))]; }
+	function randRange (min, max) { return min + (Math.random() * Math.abs(max-min)); }
+	function lerp (a, b, t) { return a + t * (b - a); }
+
 	function extend(target) {
 		for (var i = 0; i < arguments.length; i++) {
 			var source = arguments[i];
@@ -99,6 +100,32 @@
 	}
 
 
+	function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
+	  if (typeof stroke == "undefined" ) {
+	    stroke = true;
+	  }
+	  if (typeof radius === "undefined") {
+	    radius = 5;
+	  }
+	  ctx.beginPath();
+	  ctx.moveTo(x + radius, y);
+	  ctx.lineTo(x + width - radius, y);
+	  ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+	  ctx.lineTo(x + width, y + height - radius);
+	  ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+	  ctx.lineTo(x + radius, y + height);
+	  ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+	  ctx.lineTo(x, y + radius);
+	  ctx.quadraticCurveTo(x, y, x + radius, y);
+	  ctx.closePath();
+	  if (stroke) {
+	    ctx.stroke();
+	  }
+	  if (fill) {
+	    ctx.fill();
+	  }        
+	}
+
 
 
 
@@ -107,6 +134,7 @@
 		canvas: null,
 		ctx: null,
 
+		particles: [],
 		bubbles: [],
 		paddle: null,
 		wall: null,
@@ -131,6 +159,7 @@
 		},
 
 
+		fireworks: [],
 
 
 
@@ -140,6 +169,7 @@
 
 			this.initDOM();
 			this.initEvents();
+			this.initFireworks();
 
 			this.start();
 		},
@@ -168,13 +198,84 @@
 			$(window).on('mousemove', this.mouseMove);
 		},
 
+		initFireworks: function() {
+			this.fireworks.push(
+				new Animation({
+					url: 'https://mrbubblewand.files.wordpress.com/2010/07/effect_004.png',
+					xCount: 5,
+					yCount: 4,
+					width: 150,
+					height: 150
+				}),
+				new Animation({
+					url: 'https://mrbubblewand.files.wordpress.com/2010/07/effect_005.png',
+					xCount: 5,
+					yCount: 4,
+					width: 150,
+					height: 150
+				}),
+				new Animation({
+					url: 'https://mrbubblewand.files.wordpress.com/2010/07/effect_006.png',
+					xCount: 5,
+					yCount: 6,
+					width: 150,
+					height: 150
+				}),
+				new Animation({
+					url: 'https://mrbubblewand.files.wordpress.com/2010/07/effect_007.png',
+					xCount: 5,
+					yCount: 5,
+					width: 150,
+					height: 150
+				}),
+				new Animation({
+					url: 'https://mrbubblewand.files.wordpress.com/2010/07/effect_008.png',
+					xCount: 5,
+					yCount: 10,
+					width: 150,
+					height: 150
+				}),
+				new Animation({
+					url: 'https://mrbubblewand.files.wordpress.com/2010/07/effect_010.png',
+					xCount: 5,
+					yCount: 8,
+					width: 150,
+					height: 150
+				}),
+				new Animation({
+					url: 'https://mrbubblewand.files.wordpress.com/2010/07/effect_009.png',
+					xCount: 5,
+					yCount: 8,
+					width: 150,
+					height: 150
+				}),
+				new Animation({
+					url: 'https://mrbubblewand.files.wordpress.com/2010/07/effect_011.png',
+					xCount: 5,
+					yCount: 5,
+					width: 150,
+					height: 150
+				})
+			);
+			
+		},
+
 
 		start: function() {
 			this.bubbles = [];
 			this.spawner.lastSpawned = 0;
 
-			this.wall = new Rect(0, 50, 75, 550, '#333', 'fill');
+			this.wall = new Rect(0, 50, 75, 600, '#111', '#FF851B');
 			this.paddle = new Paddle(this);
+
+			// https://mrbubblewand.files.wordpress.com/2010/04/darkness_002.png   5, 6
+			// https://mrbubblewand.files.wordpress.com/2009/09/magic_003.png  5, 5
+			this.animation = new Animation().load('https://mrbubblewand.files.wordpress.com/2010/04/darkness_002.png', 5, 6);
+			// this.animation = new Animation({
+			// 	url: 'https://mrbubblewand.files.wordpress.com/2010/01/wind_002.png',
+			// 	xCount: 5,
+			// 	yCount: 6
+			// });
 
 			this.lastTime = Time.getTime();
 			this.update();
@@ -217,8 +318,12 @@
 				this.bubbleMaxSpeed+=2;
 			}
 
+			for (i = this.particles.length; i--; )
+				this.particles[i].update(time);
 
 			this.paddle.update(time);
+
+			// this.animation.update(time);
 
 			this.checkCollisions(time);
 		},
@@ -232,8 +337,13 @@
 		draw: function(time) {
 			var ctx = this.ctx;
 
-			ctx.fillStyle = '#f2f2f2';
+			// ctx.fillStyle = '#f2f2f2';
+			ctx.fillStyle = '#111';
 			ctx.fillRect(0, 0, this.width, this.height);
+
+
+			for (i = this.particles.length; i--; )
+				this.particles[i].draw(ctx);
 
 
 			for (var i = this.bubbles.length; i--; )
@@ -243,6 +353,10 @@
 			this.wall.draw(ctx);
 			this.paddle.draw(ctx);
 
+			ctx.strokeStyle = '#111';
+			ctx.strokeText("Saved: " + this.saved, this.width / 2, 10);
+			ctx.strokeText("Dropped: " + this.dropped, this.width / 2, 30);
+			ctx.strokeStyle = '#fff';
 			ctx.strokeText("Saved: " + this.saved, this.width / 2, 10);
 			ctx.strokeText("Dropped: " + this.dropped, this.width / 2, 30);
 		},
@@ -283,16 +397,82 @@
 
 			bubble.on('fall', this.bubbleFall);
 			bubble.on('goal', this.bubbleGoal);
+			bubble.on('spawn', this.bubbleSpawn);
+			bubble.on('bounce', this.bubbleBounce);
 
 			this.bubbles.push(bubble);
 		},
 
 
+		bubbleSpawn: function(bubble) {
+			var animation = new Animation({
+					x: bubble.x - bubble.radius,
+					y: bubble.y - bubble.radius,
+					width: bubble.radius * 2,
+					height: bubble.radius * 2,
+					url: 'https://mrbubblewand.files.wordpress.com/2010/01/water_001.png',
+					xCount: 5,
+					yCount: 2,
+					delay: 0.06,
+					startIndex: 0
+				})
+				.on('end', this.removeParticle);
+
+			this.particles.push(animation);
+		},
+
 		bubbleGoal: function(bubble) {
 			this.saved++;
+
+			var animation = new Animation(randItem(this.fireworks));
+			animation.x = randRange(150, 450);
+			animation.y = randRange(50, 300);
+
+			animation.on('end', this.removeParticle);
+
+			this.particles.push(animation);
 		},
 		bubbleFall: function(bubble) {
 			this.dropped++;
+
+			var animation = new Animation({
+					x: bubble.x - 50,
+					y: bubble.y - 85,
+					width: 100,
+					height: 100,
+					url: 'https://mrbubblewand.files.wordpress.com/2010/04/darkness_002.png',
+					xCount: 5,
+					yCount: 6,
+					delay: 0.06,
+					startIndex: 3
+				})
+				.on('end', this.removeParticle);
+
+			this.particles.push(animation);
+		},
+		bubbleBounce: function(bubble) {
+			/*var animation = new Animation({
+					x: bubble.x,
+					y: this.paddle.rect.y - 20,
+					width: bubble.radius,
+					height: 18,
+					url: 'https://mrbubblewand.files.wordpress.com/2011/11/heal_003.png',
+					xCount: 5,
+					yCount: 4,
+					delay: 0.02,
+					startIndex: 2,
+					endIndex: 15
+				})
+				.on('end', this.removeParticle);
+
+			this.particles.push(animation);*/
+		},
+
+		removeParticle: function(particle) {
+			var index = this.particles.indexOf(particle);
+			if (index != -1) {
+				this.particles.splice(index, 1);
+			}
 		}
 	};
 
@@ -322,23 +502,43 @@
 	///////////
 	// Rect //
 	///////////
-	function Rect(x, y, width, height, color, style) {
+	function Rect(x, y, width, height, fill, stroke, radius) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
 
-		this.color = color || null;
-		this.style = style || 'stroke';
+		this.radius = radius || null;
+
+		this.fill = fill || null;
+		this.stroke = stroke || null;
+
+		if (!this.fill && !this.stroke) {
+			this.stroke = 'black';
+		}
 	}
 	Rect.prototype.draw = function(ctx) {
 		ctx.save();
 
-		if (this.color) {
-			ctx[this.style + 'Style'] = this.color;
-		}
+		ctx.strokeStyle = this.stroke;
+		ctx.shadowBlur = 5;
+		ctx.shadowColor = this.stroke;
 
-		ctx[this.style + 'Rect'](this.x, this.y, this.width, this.height);
+		if (this.radius) {
+
+			roundRect(ctx, this.x, this.y, this.width, this.height, this.radius, this.fill, this.stroke);
+
+		} else {
+			if (this.fill) {
+				ctx.fillStyle = this.fill;
+				ctx.fillRect(this.x, this.y, this.width, this.height);
+			}
+
+			if (this.stroke) {
+				ctx.strokeStyle = this.stroke;
+				ctx.strokeRect(this.x, this.y, this.width, this.height);
+			}
+		}
 
 		ctx.restore();
 	};
@@ -359,8 +559,9 @@
 			game.height - 40,
 			width,
 			height,
-			'#333',
-			'stroke'
+			false,
+			'#FF4136',
+			10
 		);
 	}
 
@@ -380,7 +581,15 @@
 	};
 
 	Paddle.prototype.draw = function(ctx) {
+		ctx.save();
+
+		// this.rect.draw(ctx);
+		ctx.strokeStyle = this.rect.stroke;
+		ctx.shadowBlur = 5;
+		ctx.shadowColor = this.rect.stroke;
 		this.rect.draw(ctx);
+
+		ctx.restore();
 	};
 
 	Paddle.prototype.getRect = function() {
@@ -398,11 +607,29 @@
 		this.game = game;
 		this.spawn();
 
-		// https://mrbubblewand.files.wordpress.com/2010/04/darkness_002.png
-		this.animation = new Animation().load('https://mrbubblewand.files.wordpress.com/2009/09/magic_003.png', 5, 5);
+		// this.animation = new Animation({
+		// 	url: 'https://mrbubblewand.files.wordpress.com/2009/09/magic_003.png',
+		// 	xCount: 5,
+		// 	yCount: 5,
+		// 	startIndex: 2,
+		// 	endIndex: 14,
+		// 	delay: randRange(0.055, 0.075)
+		// });
+		this.animation = new Animation({
+			url: 'https://mrbubblewand.files.wordpress.com/2010/04/light_003.png',
+			xCount: 5,
+			yCount: 6,
+			sourceOffset: 40,
+			startIndex: 0,
+			endIndex: 10,
+			delay: randRange(0.055, 0.075)
+		});
+		
+		
+		var spawned = false;
 
 		this.on('bounce', function() {
-			this.vel.y *= -1;
+			this.vel.y = -Math.abs(this.vel.y * -1);
 		}, this);
 	}
 
@@ -414,9 +641,11 @@
 		this.vel = new Vec2(Math.cos(launchAngle), Math.sin(launchAngle));
 		this.velSpeed = randRange(this.game.bubbleMinSpeed, this.game.bubbleMaxSpeed);
 
-
 		this.x = 0;
 		this.y = 200;
+
+		this.angle = randRange(0, TWOPIE);
+		this.angleVel = randRange(0.5, TWOPIE);
 
 		this.velY = randRange(50, 200);
 		this.xVelTimeOffset = randRange(0, Math.PI);
@@ -428,6 +657,10 @@
 
 	Bubble.prototype.update = function(time) {
 
+		this.animation.update(time);
+
+		this.angle += this.angleVel * time.elapsedMs;
+
 		this.vel = Vec2.add(this.vel,
 						Vec2.mulVal(this.game.gravity, time.elapsedMs)
 					);
@@ -438,19 +671,31 @@
 		if (this.x > this.game.width) {
 			this.alive = false;
 			this.trigger('goal', this);
-		} else if (this.y > this.game.height) {
+		} else if (this.y > this.game.height - 10) {
 			this.alive = false;
 			this.trigger('fall', this);
+		} else if (this.x > this.game.wall.x + this.game.wall.width && !this.spawned) {
+			this.spawned = true;
+			this.trigger('spawn', this);
 		}
 	};
 
 	Bubble.prototype.draw = function(ctx) {
+		if (!this.alive) return;
+
 		ctx.save();
 
-		ctx.beginPath();
-		ctx.arc(this.x, this.y, this.radius, 0, TWOPIE, false);
-		ctx.strokeStyle = '#333';
-		ctx.stroke();
+		// ctx.beginPath();
+		// ctx.arc(this.x, this.y, this.radius, 0, TWOPIE, false);
+		// ctx.strokeStyle = '#333';
+		// ctx.stroke();
+
+
+		ctx.translate(this.x, this.y);
+		ctx.rotate(this.angle);
+
+		// this.animation.draw(ctx, this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2);
+		this.animation.draw(ctx, -this.radius, -this.radius, this.radius * 2, this.radius * 2);
 
 		ctx.restore();	
 	};
@@ -471,48 +716,124 @@
 	////////////////
 	// Animation //
 	////////////////
-	function Animation() {
-		this.spritesheet = null;
+	function Animation(opt) {
+		// Clone other Animation
+		if (opt instanceof Animation) {
+			extend(this, opt);
+
+			if (this.loaded) {
+				this.setup();
+			} else {
+				this.load(this.url);
+			}
+
+			return this;
+		}
+
+		opt = opt || {};
 		this.loaded = false;
-		this.xCount = 0;
-		this.yCount = 0;
 
-		this.width = 0;
-		this.height = 0;
+		this.spritesheet = opt.spritesheet || null;
+		this.xCount = opt.xCount || 0;
+		this.yCount = opt.yCount || 0;
 
-		this.currentIndex = 0;
-		this.delay = 0.2;
+		this.sourceOffset = opt.sourceOffset || null;
+
+		this.x = opt.x || 0;
+		this.y = opt.y || 0;
+		this.width = opt.width || 0;
+		this.height = opt.height || 0;
+
+		this.endIndex = opt.endIndex || 0;
+		this.startIndex = opt.startIndex || 0;
+		this.currentIndex = this.startIndex;
+
+		this.delay = opt.delay || 0.06;
+		this.spriteTime = this.delay;
+
+		if (opt.spritesheet) {
+			this.setup();
+		} else if (opt.url) {
+			this.load(opt.url);
+		}
 	}
 	Animation.prototype.setup = function() {
-		animation.width = this.spritesheet.width / this.xCount;
-		animation.height = this.spritesheet.height / this.yCount;
+		this.loaded = true;
+		this.spriteWidth = this.spritesheet.width / this.xCount;
+		this.spriteHeight = this.spritesheet.height / this.yCount;
+
+		this.endIndex = this.endIndex || this.xCount * this.yCount;
+		this.currentIndex = this.startIndex;
+		this.spriteTime = this.delay;
+
+		this.trigger('load', this);
 	};
-	Animation.prototype.load = function(url, xCount, yCount) {
+	Animation.prototype.load = function(url) {
 		var animation = this;
+
+		this.url = url;
 
 		var img = this.spritesheet = new Image();
 
 		this.spritesheet.onload = function() {
-			animation.loaded = true;
 			animation.setup();
 		};
-		this.spritesheet.onerror = function() {};
+		this.spritesheet.onerror = function() {
+			animation.trigger('error', animation);
+		};
 
 		this.spritesheet.src = url;
 
 		return this;
 	};
-	Animation.prototype.update = function(time) {};
-	Animation.prototype.draw = function(ctx, x, y, width, height) {
-		ctx.save();
+	Animation.prototype.update = function(time) {
+		if (!this.loaded) return;
 
-		var sx = Math.floor(this.spritesheet.width % (this.width * this.currentIndex));
-		var sy = Math.floor(this.spritesheet.width / (this.width * this.currentIndex));
+		this.spriteTime -= time.elapsedMs;
+		while (this.spriteTime <= 0.0) {
 
-		ctx.drawImage(this.spritesheet, sx, sy, this.width, this.height, x, y, width, height);
+			if (++this.currentIndex >= this.endIndex) {
+				this.trigger('end', this);
+				this.currentIndex = this.startIndex;
+			}
 
-		ctx.restore();
+			this.spriteTime += this.delay;
+		}
 	};
+	Animation.prototype.draw = function(ctx, x, y, width, height) {
+		// ctx.save();
+
+		// Only ctx sent in, use own values
+		if (arguments.length === 1) {
+			x = this.x;
+			y = this.y;
+			width = this.width;
+			height = this.height;
+		}
+
+		if (this.loaded) {
+			var sw = this.spriteWidth;
+			var sh = this.spriteHeight;
+			var sx = Math.floor((sw * this.currentIndex) % this.spritesheet.width);
+			var sy = Math.floor((sw * this.currentIndex) / this.spritesheet.width) * sh;
+
+			if (this.sourceOffset) {
+				sx += this.sourceOffset;
+				sy += this.sourceOffset;
+				sw -= this.sourceOffset * 2;
+				sh -= this.sourceOffset * 2;
+			}
+
+			ctx.drawImage(this.spritesheet, sx, sy, sw, sh, x, y, width, height);
+			
+			// ctx.strokeStyle = 'green';
+			// ctx.strokeRect(x, y, width, height);
+		}
+	};
+
+	extend(Animation.prototype, Events);
+
+
 
 
 
